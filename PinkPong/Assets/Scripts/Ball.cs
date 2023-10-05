36867bedb3142be9463fce1fae66bcf78e727058
lizaveta kryvitskaya <144534042+d1onaea@users.noreply.github.com>
2023-10-05 16:50:43 +0300

    Another fixes

--- PinkPong/Assets/Scripts/Ball.cs
+++ PinkPong/Assets/Scripts/Ball.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    [SerializeField] private Rigidbody2D rigidbody;
+    [SerializeField] private new Rigidbody2D rigidbody;
     [SerializeField] private float maxInitAngle = 0.67f;
     [Range(0, 10)][SerializeField] private float moveSpeed = 1f;
     [SerializeField] private float speedMultiplier = 1.1f;
@@ -19,7 +19,7 @@ public class Ball : MonoBehaviour
     {
         Vector2 direction = Random.value < 0.5f ? Vector2.left : Vector2.right;
         direction.y = Random.Range(-maxInitAngle, maxInitAngle);
-        rb2d.velocity = direction * moveSpeed;
+        rigidbody.velocity = direction * moveSpeed;
     }
 
     private void ResetBall()
@@ -36,37 +36,4 @@ public class Ball : MonoBehaviour
     }
 }
 
-//ÿ õç êàê ýòî ñäåëàòü àäåêâàòíî ÷åñòíî 
-//private void OnTriggerEnter2D(Collider2D collision)
-//{
-//    if (collision.gameObject.name == "ScoreZone")
-//    {
-//        Paddle paddle = collision.GetComponent<Paddle>();
-//        if (paddle)
-//        {
-//            GameManager.Instance.OnScoreZoneReached(paddle.ID);
-//        }
-//        else
-//        {
-//            rb2d = GetComponent<Rigidbody2D>();
-//            if (rb2d.velocity.x > 0)
-//            {
-//                GameManager.Instance.OnScoreZoneReached(1);
-//            }
-//            else
-//            {
-//                GameManager.Instance.OnScoreZoneReached(2);
-//            }
-//        }
-//    }
-//}
 
-//    private void OnCollisionEnter2D(Collision2D collision)
-//    {
-//        Paddle paddle = collision.collider.GetComponent<Paddle>();
-//        if (paddle)
-//        {
-//            rb2d.velocity *= speedMultiplier;
-//        }
-//    }
-//}
