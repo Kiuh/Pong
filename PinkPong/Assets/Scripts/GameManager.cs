9f888b1cfbd2646bfa9c26a2eb13b9b9fc16d987
lizaveta kryvitskaya <144534042+d1onaea@users.noreply.github.com>
2023-10-07 01:15:27 +0300

    Completed the game

--- PinkPong/Assets/Scripts/GameManager.cs
+++ PinkPong/Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -22,14 +23,29 @@ public class GameManager : MonoBehaviour
     private PaddleMovement lastHitPaddle;
 
     [SerializeField]
+    private GameObject gameEnd;
+
+    [SerializeField]
+    private TMP_Text win;
+
+    [SerializeField]
     private Ball ball;
+
+    [SerializeField]
+    private GameObject pause;
+
+    [SerializeField]
+    private int maxScore;
 
     private bool locker = false;
 
     // Called by Ball
     public void OnPaddleCollide(PaddleMovement paddle)
     {
+        paddle1.SetNormalTransparency();
+        paddle2.SetNormalTransparency();
         lastHitPaddle = paddle;
+        paddle.SetTransparent();
         ball.IncreaseSpeed();
     }
 
@@ -48,6 +64,12 @@ public class GameManager : MonoBehaviour
         }
         locker = false;
         UpdateScore();
+        if (scorePlayer1 == maxScore || scorePlayer2 == maxScore)
+        {
+            EndGame();
+        }
+        paddle1.SetNormalTransparency();
+        paddle2.SetNormalTransparency();
     }
 
     private void Update()
@@ -57,20 +79,61 @@ public class GameManager : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 lastHitPaddle = paddle2;
+                paddle2.SetTransparent();
                 ball.AddForce();
                 locker = true;
             }
             if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift))
             {
+                paddle1.SetTransparent();
                 lastHitPaddle = paddle1;
                 ball.AddForce();
                 locker = true;
             }
         }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            OnPause();
+        }
     }
+
     private void UpdateScore()
     {
         scoreTextLeft.SetScore(scorePlayer1);
         scoreTextRight.SetScore(scorePlayer2);
+    }
+
+    private void EndGame()
+    {
+        gameEnd.SetActive(true);
+        win.text = scorePlayer1 == maxScore ? "Player 1 won!" : "Player 2 won!";
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void RestartGame()
+    {
+        gameEnd.SetActive(false);
+        ball.ResetBallPosition();
+        ball.ResetVelocity();
+        paddle1.ResetPosition();
+        paddle2.ResetPosition();
+        scorePlayer1 = scorePlayer2 = 0;
+        UpdateScore();
+    }
+    public void OnPause()
+    {
+        Time.timeScale = 0;
+        pause.SetActive(true);
+    }
+
+    public void Continue()
+    {
+        Time.timeScale = 1;
+        pause.SetActive(false);
     }
 }
--- PinkPong/Assets/Scripts/PaddleMovement.cs
+++ PinkPong/Assets/Scripts/PaddleMovement.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class PaddleMovement : MonoBehaviour
@@ -6,7 +7,14 @@ public class PaddleMovement : MonoBehaviour
     private float speed;
 
     [SerializeField]
-    private float dashDistance;
+    private float dashTime;
+
+    [SerializeField]
+    private float dashSpeed;
+
+    private float dashTimer;
+
+    private RotationDirection dashRotateDirection;
 
     [SerializeField]
     private Transform target;
@@ -14,15 +22,21 @@ public class PaddleMovement : MonoBehaviour
     [SerializeField]
     private PaddleInput paddleInput;
     private Vector3 startPosition;
+    private Quaternion startRotation;
+
+    [SerializeField] private TMP_Text dashCD;
 
     [SerializeField]
     private float dashCoolDown;
 
+    [SerializeField] private SpriteRenderer sprite;
+
     private float timer;
 
-    private void Start()
+    public void Awake()
     {
         startPosition = transform.position;
+        startRotation = transform.rotation;
     }
 
     private void Update()
@@ -30,12 +44,29 @@ public class PaddleMovement : MonoBehaviour
         if (timer > 0)
         {
             timer -= Time.deltaTime;
+            dashCD.text = ((int)timer).ToString();
+        }
+        else
+        {
+            dashCD.text = "Dash ready!";
+        }
+        dashTimer -= Time.deltaTime;
+        if (dashTimer > 0)
+        {
+            transform.RotateAround(
+               target.position,
+               Vector3.forward,
+               Time.deltaTime
+                   * dashSpeed
+                   * (dashRotateDirection == RotationDirection.CounterClockWise ? -1 : 1)
+           );
         }
     }
 
     public void ResetPosition()
     {
         transform.position = startPosition;
+        transform.rotation = startRotation;
     }
 
     public void Move(RotationDirection rotateDirection)
@@ -55,11 +86,18 @@ public class PaddleMovement : MonoBehaviour
         {
             return;
         }
-        transform.RotateAround(
-            target.position,
-            Vector3.forward,
-            dashDistance * (rotateDirection == RotationDirection.CounterClockWise ? -1 : 1)
-        );
+        dashRotateDirection = rotateDirection;
+        dashTimer = dashTime;
         timer = dashCoolDown;
+    }
+
+    public void SetTransparent()
+    {
+        sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, 0.3f);
+    }
+
+    public void SetNormalTransparency()
+    {
+        sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, 1f);
     }
 }
